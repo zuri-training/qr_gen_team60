25cce93942df43731baee78bbb839c8db6704b48
Avong <79746477+av215@users.noreply.github.com>
2022-07-31 05:45:21 +0100

    Second Updated Commit
    second commit with working linking,
    that leads to the corresponding pages
    when clicked

--- qr_gen_project/qr_gen_project/urls.py
+++ qr_gen_project/qr_gen_project/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-#from django.conf.urls import include, re_path as url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
