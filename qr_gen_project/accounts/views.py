0656200cacef80b5f9421ee23e24abb1ebbbc8d5
Avong <79746477+av215@users.noreply.github.com>
2022-07-31 03:30:57 +0100

    update for pages features

---
+++ qr_gen_project/accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .models import Qr_Collection
+from django import forms
+
+# Create your views here.
+def index(request):
+    return render(request, 'accounts/index.html')
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm()
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, your registration is successful.')
+            return redirect('login')
+        else:
+            form = UserCreationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
+     
--- qr_gen_project/qr_gen_project/urls.py
+++ qr_gen_project/qr_gen_project/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+#from django.conf.urls import include, re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
 ]
