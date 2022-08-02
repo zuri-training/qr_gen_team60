40b10846ab10b69f5331b24f2dcbf13c5240c8c6
Tope-stack <yusufftope70@gmail.com>
2022-08-02 09:40:07 +0100

    created new views and urls to generate qr-code

--- qr_gen_project/qr_generator/views.py
+++ qr_gen_project/qr_generator/views.py
@@ -1,7 +1,20 @@
 # account/views.py
 
 from django.shortcuts import render
+from django.conf import settings
+from qrcode import *
+import time 
 
 def index(request):
     context = {}
     return render(request, 'qr_generator/index.html', context)
+
+
+def qr_gen(request):
+    if request.method == 'POST':
+        data = request.POST['data']
+        img = make(data)
+        img_name = 'qr' + str(time.time()) + '.png'
+        img.save(str(settings.MEDIA_ROOT) + '/' + img_name)
+        return render(request, 'index.html', {'img_name': img_name})
+    return render(request, 'index.html')
