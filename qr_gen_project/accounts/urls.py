805c8711b60bc8a91ce090e995f74dc8aa7126a7
Avong <79746477+av215@users.noreply.github.com>
2022-08-02 12:54:26 +0100

    update
    updated the login, logout views, urls, and tempalte

--- qr_gen_project/accounts/urls.py
+++ qr_gen_project/accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path('', views.index, name="index"),
     path('register/',views.register, name="register"),
-    path('login/',views.login, name="login"),
+    path('login/',views.login_view, name="login"),
     path('logout/',views.logout, name="logout"),
     path('dashboard/',views.dashboard, name="dashboard"),
 ]
