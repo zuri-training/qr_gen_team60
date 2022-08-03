d3c687fabca5141fbf9258aa5f93e77b2f84a39b
Wisdom Emmanuel <simplenick01@gmail.com>
2022-08-03 17:32:37 +0100

    Fixed CSS issue

--- qr_gen_project/qr_generator/urls.py
+++ qr_gen_project/qr_generator/urls.py
@@ -7,23 +7,5 @@ from qr_generator import views
 app_name = 'qr_generator'
 urlpatterns = [
     path('', views.index, name="home"), 
-]
-
-urlpatterns +=[
-    path('test', views.qr_gen, name="home"),
-]
-
-<<<<<<< HEAD
-
-urlpatterns +=[
-    path('contact', views.contact_us, name='contact'),
-]
-
-urlpatterns +=[
-    path('nick', views.qr_gen2, name="test"),
-    path('<str:template>/', views.form, name='form'),
-=======
-urlpatterns +=[
-    path('<str:template>/', views.test_form, name='form'),
->>>>>>> a84a2e4572177ea61c7c5789f04c087874aa5d86
+    path('contact-us/', views.contact_us, name='contact'),
 ]
